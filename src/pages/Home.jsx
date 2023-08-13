fb9930c4cb189e2250b47273f30852f497ae37cf
Donald Dominic <gotodonald@gmail.com>
2023-08-14 00:57:28 +0100

    Add home page header text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,25 @@
-import { useEffect } from "react";
+import logo from '../assets/logo.png'
 
 const Home = () => {
-    useEffect(() => {
-        throw new Error("Invalid state");
-
-    }, []);
+    
     return (
-        <div>
-            <h1>BEST STORE</h1>
+      <div className="px-8 md:px-32 glass">
+        <div className="py-8">
+          <img src={logo} alt="best store logo" width={40} height={60} />
         </div>
-    )
+        <div className="grid grid-cols-3">
+          <div className="col-span-3 font-bold text-6xl text-green-700 pb-4 text-center">
+            Easy buy{" "}
+          </div>
+          <div className="col-span-3 font-bold text-4xl pb-4 text-center">
+            on my{" "}
+          </div>
+          <div className="col-span-3 font-extrabold text-6xl text-blue-700 text-center">
+            BEST STORE
+          </div>
+        </div>
+      </div>
+    );
 }
 
 export default Home;
