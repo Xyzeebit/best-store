e6db9b11ef96367629595b40f977d68378b6a875
Donald Dominic <gotodonald@gmail.com>
2023-09-08 22:35:08 +0100

    Add deals item component

--- src/components/core/DealsOfTheDay.jsx
+++ src/components/core/DealsOfTheDay.jsx
@@ -1,3 +1,10 @@
+import DealsItem from "./DealsItem";
+import imgMakeup from '../../assets/makeup.jpg';
+import imgStationery from "../../assets/stationery.jpg";
+import imgFootwear from "../../assets/footwear.jpg";
+import imgToys from "../../assets/toys.jpg";
+
+
 
 const DealsOfTheDay = () => {
     return (
@@ -7,10 +14,34 @@ const DealsOfTheDay = () => {
             Deals of the Day
           </div>
           <div className="grid grid-cols-4 gap-4">
-            <div className="col-span-2 h-36 bg-white rounded-lg"></div>
-            <div className="col-span-2 h-36 bg-white rounded-lg"></div>
-            <div className="col-span-2 h-36 md:h-32 bg-white rounded-lg"></div>
-            <div className="col-span-2 h-36 md:h-32 bg-white rounded-lg"></div>
+            <DealsItem
+              id={"jkakilOOij"}
+              text={"Makeup kit from top brand"}
+              image={imgMakeup}
+              price={"195.99"}
+              styles={"col-span-2 h-36 bg-white rounded-lg shadow-md"}
+            />
+            <DealsItem
+              id={"jkakilvOOij"}
+              text={"Get your stationeries for half the price"}
+              image={imgStationery}
+              price={"15.45"}
+              styles={"col-span-2 h-36 bg-white rounded-lg shadow-md"}
+            />
+            <DealsItem
+              id={"jkakixlOOij"}
+              text={"Mens footwear from top brand"}
+              image={imgFootwear}
+              price={"250.00"}
+              styles={"col-span-2 h-36 bg-white rounded-lg shadow-md"}
+            />
+            <DealsItem
+              id={"jkakilOvOij"}
+              text={"Kiddies special"}
+              image={imgToys}
+              price={"5.99"}
+              styles={"col-span-2 h-36 bg-white rounded-lg shadow-md"}
+            />
           </div>
         </div>
         <div className="col-span-3 md:col-span-1 rounded-lg">
