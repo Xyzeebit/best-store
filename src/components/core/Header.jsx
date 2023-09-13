490a13ea3041cd40ddf560cc482bef9566330707
Donald Dominic <gotodonald@gmail.com>
2023-09-14 00:27:27 +0100

    Implement login and account options - Add sign in link to header component - Add user reducer to check logins

--- src/components/core/Header.jsx
+++ src/components/core/Header.jsx
@@ -5,12 +5,15 @@ import LinkIconButton from './LinkIconButton';
 import Wrapper from './Wrapper';
 
 import personIcon from '../../assets/icons/user_icon.svg';
+import loginIcon from "../../assets/icons/login_icon.svg";
+
 import SearchList from './SearchList';
 import { useSelector } from 'react-redux';
 import Logo from './Logo';
 
 const Header = () => {
-    const { cartItemCounter } = useSelector(state => state.categories);
+  const { cartItemCounter } = useSelector(state => state.categories);
+  const { isLoggedIn } = useSelector(state => state.user);
     return (
       <div className='sticky top-0 z-50 '>
         <div className="bg-yellow-500 h-4"></div>
@@ -18,7 +21,11 @@ const Header = () => {
           {/* <NavBrand brand="Bestore" /> */}
           <Logo />
           <Wrapper>
-            <LinkIconButton text={"Account"} icon={personIcon} to="/account" />
+            <LinkIconButton 
+              text={`${isLoggedIn ? "Account" : "Sign in"}`} 
+              icon={isLoggedIn ? personIcon : loginIcon} 
+              to={`${isLoggedIn ? "/account" : "/signin"}`} 
+            />
             <Cart items={cartItemCounter} />
           </Wrapper>
           <ProductSearch />
--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,10 +1,12 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import categoriesSlice from "./categoriesSlice";
 import topCategoriesReducer from "./topcatSlice";
+import userReducer from "./userSlice";
 
 const rootReducer = combineReducers({
     topCategories: topCategoriesReducer,
     categories: categoriesSlice,
+    user: userReducer,
 });
 
 export const store = configureStore({
