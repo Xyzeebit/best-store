1c2c3b9d7736efcd2add4c745eccb7855d5ebb58
Donald Dominic <gotodonald@gmail.com>
2023-10-19 19:32:41 +0100

    Add style classes to item component

--- src/components/products/Item.jsx
+++ src/components/products/Item.jsx
@@ -2,30 +2,41 @@ import Rating from '../core/Rating'
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-const Item = ({ id, text, price, image, ratings }) => {
+const Item = ({ id, title, price, image, ratings, category }) => {
     return (
-      <div>
-        <div>
-          <img src={image} alt={text} width={150} height={100} />
+      <div className="shadow rounded overflow-hidden">
+        <div className="h-32 overflow-hidden">
+          <img
+            src={image}
+            alt={title}
+            width={150}
+            height={100}
+            className="object-fit w-full h-32"
+          />
         </div>
-        <div>
-          <Link to={`/products/${id}`}>{text}</Link>
-          <p>{price}</p>
-          <div />
-          <div>
+        <div className="p-2">
+          <div className="truncate font-semibold text-sm text-green-900 hover:underline">
+            <Link to={`/${category}/${id}`}>{title}</Link>
+          </div>
+          <p className="text-red-500">USD {price}</p>
+          <div className="text-sm text-gray-500">
             {ratings ? (
               <div>
                 <small>Reviews</small>
+                <div className="py-[1px] bg-gray-300 mb-[4px]" />
                 <Rating rating={ratings} />
               </div>
             ) : (
               <div>
                 <small>No reviews</small>
+                <div className="py-[1px] bg-gray-300 mb-[4px]" />
               </div>
             )}
           </div>
-          <div>
-            <button>Add to Cart</button>
+          <div className="flex items-center justify-center pt-2">
+            <button className="font-semibold text-xsm text-gray-500 px-4 py-1 rounded-3xl bg-white border-2 border-gray-400 hover:bg-red-500 hover:border-red-500 hover:text-white">
+              Add to Cart
+            </button>
           </div>
         </div>
       </div>
@@ -34,9 +45,10 @@ const Item = ({ id, text, price, image, ratings }) => {
 
 Item.propTypes = {
     id: PropTypes.string.isRequired,
-    text: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
     image: PropTypes.string.isRequired,
     price: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
     ratings: PropTypes.number,
 };
 
