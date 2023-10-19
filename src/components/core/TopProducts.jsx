b42af53504494d60da94700aa9479b37ba6d55b4
Donald Dominic <gotodonald@gmail.com>
2023-10-19 17:10:58 +0100

    Change product link hover color

--- src/components/core/TopProducts.jsx
+++ src/components/core/TopProducts.jsx
@@ -1,5 +1,6 @@
 import { NavLink } from "react-router-dom";
 import PropTypes from 'prop-types'
+import ProductList from "../products/ProductList";
 
 
 const PRODUCTS = [
@@ -62,16 +63,17 @@ const PRODUCTS = [
 ];
 
 const TopProducts = () => {
-    return (
-        <div className="px-4 md:px-16 py-4">
-            <h1 className="font-bold text-xl text-green-900 pb-2">Top Product Categories</h1>
-            <div className="py-4 flex items-start justify-start gap-4 overflow-auto top-pro">
-                {PRODUCTS.map(({ text, url }) => (
-                    <ProductLink key={text + url} text={text} url={url} />
-                )) }
-            </div>
-        </div>
-    );
+  return (
+    <div className="px-4 md:px-16 py-4">
+      <h1 className="font-bold text-xl text-green-900 pb-2">Top Product Categories</h1>
+      <div className="py-4 flex items-start justify-start gap-4 overflow-auto top-pro">
+        {PRODUCTS.map(({ text, url }) => (
+          <ProductLink key={text + url} text={text} url={url} />
+        ))}
+      </div>
+      <ProductList />
+    </div>
+  );
 }
 
 const ProductLink = ({ text, url }) => (
@@ -79,8 +81,8 @@ const ProductLink = ({ text, url }) => (
     to={url}
     className={({ isActive }) =>
       isActive
-        ? "bg-green-900 text-white rounded-3xl px-4 py-1"
-        : "text-green-900 text-xsm font-bold bg-white hover:bg-green-900 hover:text-white border-2 border-green-900 rounded-3xl px-4 py-1"
+        ? "bg-green-900 text-white rounded-3xl px-4 py-1 shadow"
+        : "text-green-900 text-xsm font-bold bg-white hover:bg-red-500 hover:border-red-500 hover:text-white border-2 border-green-900 rounded-3xl px-4 py-1 shadow"
     }
   >
     {text}
