366ec65328b2e68a613607425e65c5a4f3171a74
Donald Dominic <gotodonald@gmail.com>
2023-12-03 19:35:37 +0100

    Implement checkout functionality in cart component

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,12 +1,35 @@
 import { useEffect } from "react";
 import { Layout } from "../components/core";
-import { Link } from 'react-router-dom';
-import { useSelector } from "react-redux";
+import { useNavigate } from 'react-router-dom';
+import { useSelector, useDispatch } from "react-redux";
 import CartList from "../components/cart/CartList";
 import cartIcon from "../assets/icons/cart-large_icon.svg";
+import { createOrders } from "../redux/categoriesSlice";
 
 const Cart = () => {
   const { cart } = useSelector((state) => state.categories);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleCheckout = () => {
+    const order = {
+      orders: [],
+      deliveryDetails: {},
+      shippingCost: 0.0,
+    };
+    if (cart && cart.length > 0) {
+      cart.forEach(item => {
+        order.orders.push({
+          id: item.id,
+          title: item.title,
+          price: item.prices[0],
+          quantity: item.quantity,
+        });
+      });
+    }
+    dispatch(createOrders(order));
+    navigate("/checkout")
+  }
 
   useEffect(() => {
     document.title = "Bestore | Cart";
@@ -18,7 +41,7 @@ const Cart = () => {
         {cart.length > 0 ? (
           <>
             <div className="flex justify-end items-center mb-8">
-              <Link to="/checkout" className="text-white bg-green-900 font-semibold px-8 py-2 rounded-3xl shadow hover:bg-red-500">Checkout</Link>
+              <button onClick={handleCheckout} className="text-white bg-green-900 font-semibold px-8 py-2 rounded-3xl shadow hover:bg-red-500">Checkout</button>
             </div>
             <CartList items={cart} />
           </>
